b628dc1ab64b31a5d4eacc48844401eb9c363167
Noel Chiasson <noel@discoverygarden.ca>
2022-07-05 16:31:11 -0300

    Removing unnecessary allow.plugins

--- lint.sh
+++ lint.sh
@@ -4,9 +4,7 @@ set -e #Exit entire script if any command fails
 
 # Download dependencies
 echo "Allowing plugin dependencies ..."
-composer global config --no-interaction allow-plugins.drupal/coder true
 composer global config --no-interaction allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
-composer global config --no-interaction allow-plugins.sebastian/phpcpd true
 echo "Downloading dependencies ..."
 composer global require drupal/coder
 composer global require dealerdirect/phpcodesniffer-composer-installer
